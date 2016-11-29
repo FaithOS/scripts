101673880b4570596a9e85b17a7c4edcbe753c6b
wjg <820007989@qq.com>
2016-11-29 18:04:28 +0800

    自动挂载磁盘

--- disk.sh
+++ disk.sh
@@ -1,50 +1,59 @@
-#!/bin/sh
-DIS_LIST=`fdisk -l |awk -F " |:" '{if ($1~/Dis/) print $2 }' |egrep -v 'identifier' `
-SYS_DIS=`fdisk -l | egrep "^\/" |awk '{if ($2~/\*/) print $1 }'`
-SYS_NUM=`fdisk  -l | grep  "${SYS_DIS%[[:digit:]]}"| wc -l`
-SYS_DIS_TWO=` df -h  | sed -n '2p' |awk '{print $1}'`
+#/bin/bash
+#########################################
+#Function:    auto mount  disk
+#Usage:       bash disk_2016-11-29.sh
+#Author:      Faith
+#OICQ：       820007989
+#Blog：       jianguoa.wang
+#Company:     XX
+#Version:     1.0
+#Date：       2016-11-29
+#########################################
+#该脚本适应于cenos 和ubuntu系统
+SYS_DIS=`fdisk -l 2>&1 | egrep "^\/" |awk '{if ($2~/\*/) print $1 }'`
+SYS_DIS_TWO=`df -h  | grep 'boot' |awk '{print $1}'`
+DISK_SUM=(`lsblk -l | awk '{if ($6~/disk/) print $1}'`)
+echo ====
 if   [ ${SYS_DIS}  == $SYS_DIS_TWO  ] ;then
     echo  "确认系统盘为 ： ${SYS_DIS%[[:digit:]]} 。"
 fi
-A=`echo  "${DIS_LIST}" |sed -n '2p' `
-#b=`fdisk -l | grep ${A%[[:digit:]]}`
-b=`fdisk -l | grep '/dev/xvdb' |sed -n '2p' |awk  '{print $1}'`
-if [ -n "$A" -a  -z "$b"  ];then
-    echo  " ${A} 是数据盘, 可以初始化。 "
-fdisk $A <<EOF
-N
-P
+
+A=`echo "${SYS_DIS%[[:digit:]]}"|awk -F '/' '{print $3}'`
+##判断谁是数据盘
+DISK_TOTL=`lsblk -l | awk '{if ($6~/disk/) print $1}'|wc -l`
+for ((a=0;a<="$DISK_TOTL";a++));do
+    if [ "${DISK_SUM[$a]}" != "$A" ];then
+    B="${DISK_SUM[$a]}"
+    BB=`fdisk -l 2>&1 | grep "${DISK_SUM[$a]}" |awk -F '[ :]' '{print $2}'`
+##判断数据盘是否被格式化过
+    DATA=`fdisk -l 2>&1 | grep "$B" |wc -l`
+        if [ $DATA  -le 1  ];then
+            echo "${BB} 是数据盘可以初始化。"
+fdisk $BB <<EOF 2>&1
+n
+p
 1
 
 
-W
+w
 EOF
-b=`fdisk -l | grep '/dev/xvdb' |sed -n '2p' |awk  '{print $1}'`
-mkfs.ext4  $b
-    if [ $?  -eq 0 ] ;then
-        if [ ! -d  /data  ];then
-            mkdir /data
-            UUID=`blkid $b  |awk -F ' ' '{print $2}'`
-	    echo  "  $UUID       /data      ext4    defaults        0       0"  >>/etc/fstab
-	    mount -a 
-	        if [ $? -eq  0   ];then
-    		    echo -e "\033[31m 磁盘挂载成功。\033[0m"
-	        fi
+	    C=`fdisk -l 2>&1 | grep "${BB}[[:digit:]]" |awk '{print $1}'`
+	    mkfs -t ext4 $C 2>&1
+	    if [ $? -eq 0  ];then
+		if [ ! -d /data"${a}"  ];then
+		    mkdir /data"${a}"
+		    UUID=`blkid "${C}"|awk -F ' ' '{print $2}'`
+		    echo  "  $UUID       /data$a      ext4    defaults        0       0"  >>/etc/fstab	   
+		    mount -a 
+			if [ $? -eq  0   ];then
+    		    	    echo -e "\033[31m 磁盘挂载成功。\033[0m"
+	        	fi
+		fi
+	    fi
+        elif [ $a -eq $DISK_TOTL  ];then
+            break
         else
-            echo "data is already existed."
-            echo "plese check ."
-            exit 1
+            echo "${DISK_SUM[$a]}  已经初始化过了。"
         fi
     fi
-else
-      if [ $b != ${SYS_DIS%[[:digit:]]}  ];then 
-          echo "${b} 和 ${SYS_DIS%[[:digit:]]}   不是同一块盘,可以初始化磁盘  "
- 	     if [ -n $A  ];then
-		 echo -e "\033[31m [Warning] "$0" 退出，因为, 数据盘可能已经初始化过了，或者没有数据盘"
-		 echo -e "\033[0m "
-     	     fi
-      fi
-fi
-
-
-
+done
