9f080253c693c72af088d752cb5a22b682dc6ee6
Faith <820007989@qq.com>
2016-11-30 15:05:02 +0800

    ubuntu下可以执行

--- init_git.sh
+++ init_git.sh
@@ -5,12 +5,12 @@ echo $basedir
 git config --global user.name "Faith"
 git config --global user.email 820007989@qq.com
 if [  -f $basedir/.git/config   ];then
- /bin/cp $basedir/.git/config $basedir/.git/config_(`data` +%F).bak 
+ /bin/cp $basedir/.git/config $basedir/.git/config_`date +%F`.bak 
 GITHUB=`grep github $basedir/.git/config  | awk '{print $3}'`
     if [ $MY_github == GITHUB  ];then 
         echo 'git is ok'
     else
-        sed -i s/github.com/FaithOS@/g $basedir/.git/config
+        sed -i s/github/FaithOS@github/g $basedir/.git/config
     fi
 else
     echo 'git config file is not exist'
