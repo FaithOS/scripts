e760383b81f14bc2f367c7259abc7b615a3cbb26
root <root@192-168-139-121.(none)>
2016-12-05 13:35:47 +0800

    修改 关闭IPv6 bug

--- init_sys.sh
+++ init_sys.sh
@@ -13,7 +13,6 @@ fi
 
 DISABLED_ipv6(){
 cat >> /etc/sysctl.conf<<EOF 
-root@ubuntu:~#  sudo sysctl -p 
 net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
