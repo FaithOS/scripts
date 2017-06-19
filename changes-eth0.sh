cb582cd89384e796da9fcccc6ced1986950fb98e
Faith <820007989@qq.com>
2017-06-19 00:01:18 -0400

    changes-eth0.sh 判断系统是否为eth0 ，如果是就不执行该脚本

--- changes-eth0.sh
+++ changes-eth0.sh
@@ -7,6 +7,11 @@
 ETH0=`ip addr  | awk  -F '2:' '{print $2}' |awk  -F ':' '{print $1}'  | awk NF|awk '{sub("^ *","");sub(" *$","");print}'  |sed -n 1p`
 ATTR=`ip addr |grep -C 1 "$ETH0": |sed -n 3p |awk -F ' ' '{print $2}'`
 
+if  [ $ETH0  == eth0  ];then
+    echo "网卡为eth0 ， 不需要修改"    
+    exit 0
+fi
+
 if [ -f  /etc/sysconfig/network-scripts/ifcfg-${ETH0}  ];then
 mv /etc/sysconfig/network-scripts/ifcfg-${ETH0} /tmp
 cat >> /etc/sysconfig/network-scripts/ifcfg-eth0<< EOF
@@ -29,6 +34,7 @@ if [ -f  /etc/default/grub.bak  ];then
     grub2-mkconfig -o /boot/grub2/grub.cfg
 else
     echo  'grub 没有备份成功。'
+    exit 0
 fi
 
 
@@ -36,7 +42,7 @@ echo "SUBSYSTEM=="net",ACTION=="add",DRIVERS=="?*",ATTR{address}=="$ATTR",ATTR{t
 
 else
     echo no
-
+    exit 0
 fi
 
 route add  default gw 172.16.31.254
