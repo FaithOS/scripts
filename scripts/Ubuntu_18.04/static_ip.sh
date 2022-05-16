01620795071ee455698b5b0420102bbdad69fd53
FaithOS <273101165@qq.com>
2022-05-16 13:37:26 +0800

    完善修改静态IP 的缩进

--- scripts/Ubuntu_18.04/static_ip.sh
+++ scripts/Ubuntu_18.04/static_ip.sh
@@ -9,15 +9,16 @@ cp -r /etc/netplan/00-installer-config.yaml  /etc/netplan/00-installer-config.ya
 ###cat >/file.txt<<-EOF #这里的-EOF 表述如果EOF 后面有空格也作为制表符，注意：EOF前后都不应有空格或其他符号。
 ###EOF
 cat >/etc/netplan/00-installer-config.yaml<<-EOF
+# This is the network config written by 'subiquity'
 network:
   ethernets:
     ${INTER_NAME}:
-         dhcp4: no
-         addresses: [${INTER_IP}]
-         optional: true
-         gateway4: ${GATEWAY}
-         nameservers:
-       	       addresses: [114.114.114.114]
+      dhcp4: no
+      addresses: [${INTER_IP}]
+      optional: true
+      gateway4: $GATEWAY
+      nameservers:
+              addresses: [114.114.114.114]
   version: 2
 EOF
 
