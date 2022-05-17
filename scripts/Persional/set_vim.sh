d608fce23a748d849bfde5ab0ff71164bb484996
FaithOS <273101165@qq.com>
2022-05-17 16:04:19 +0800

    完善vim 脚本

--- scripts/Persional/set_vim.sh
+++ scripts/Persional/set_vim.sh
@@ -4,7 +4,11 @@
 
 #
 #备要修改的文件
-if [   -e /etc/vim/vimrc.local    ]
+if [ !  -e /etc/vim/vimrc.local    ];then
+touch /etc/vim/vimrc.local
+echo '"设置vim粘贴格式' >>/etc/vim/vimrc.local
+echo 'set paste' >> /etc/vim/vimrc.local
+else
  /bin/cp -r /etc/vim/vimrc.local /etc/vim/vimrc.local-`date +%F`
 echo '"设置vim粘贴格式' >>/etc/vim/vimrc.local
 echo 'set paste' >> /etc/vim/vimrc.local
--- scripts/Persional/set_vim_tab.sh
+++ scripts/Persional/set_vim_tab.sh
@@ -4,9 +4,13 @@
 
 #
 #备要修改的文件
-if [   -e /etc/vim/vimrc.local    ]
+if [ !  -e /etc/vim/vimrc.local    ];then
+touch /etc/vim/vimrc.local
+echo '"设置vim的tab键四个空格' >>/etc/vim/vimrc.local
+echo 'set paste\nset shiftwidth=4\set shiftwidth=4\n'  >> /etc/vim/vimrc.local
+else
  /bin/cp -r /etc/vim/vimrc.local /etc/vim/vimrc.local-`date +%F`
 echo '"设置vim的tab键四个空格' >>/etc/vim/vimrc.local
-echo 'set paste\nset shiftwidth=4\set shiftwidth=4\n' >> /etc/vim/vimrc.local
+echo 'set paste\nset shiftwidth=4\set shiftwidth=4\n'  >> /etc/vim/vimrc.local
 fi
 echo "vim set is  ok " 
