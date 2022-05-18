fc6c3a931b6d6848b571e5409f1db4dc40e63481
FaithOS <273101165@qq.com>
2022-05-18 17:22:16 +0800

    注释掉history的日志

--- scripts/Persional/set_history.sh
+++ scripts/Persional/set_history.sh
@@ -5,7 +5,7 @@
 #历史命令记录日志
 #禁止cat 解析变量
 cat >/etc/profile.d/history.sh<<-"EOF"
-HISTFILE=/vat/log/history.log 
+#HISTFILE=/vat/log/history.log 
 #由于bash的history文件默认是覆盖，如果存在多个终端，最后退出的会覆盖以前历史记录，改为追加形式
 shopt -s histappend
 ###
