dd411e2ae90065f3f42c89d8405de26e5580ea25
FaithOS <273101165@qq.com>
2022-05-18 09:28:23 +0800

    完善apt-get配置源,cat的EOF格式

--- scripts/Ubuntu_18.04/apt-source.sh
+++ scripts/Ubuntu_18.04/apt-source.sh
@@ -4,7 +4,7 @@
 #
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
 
-cat >/etc/apt/sources.list<-EOF
+cat >/etc/apt/sources.list<<-EOF
 # 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
