83b2129b176734cc61612cce2e26b62307d89284
FaithOS <273101165@qq.com>
2022-05-25 14:59:36 +0800

    完善环境变量的IP 地址

--- scripts/main_profile/my_profile.sh
+++ scripts/main_profile/my_profile.sh
@@ -5,7 +5,8 @@ export OS_VERSION=`awk -F '=|"' '/^VERSION_ID/{print $3}' /etc/os-release`
 export OS_NAME_VERSION=${OS_NAME}_${OS_VERSION}
 #网卡检查
 export INTER_NAME=`ip addr  | awk  -F '2:' '{print $2}' |awk  -F ':' '{print $1}'  | awk NF|awk '{sub("^ *","");sub(" *$","");print}'  |sed -n 1p`
-export INTER_IP=`ip addr | grep  ens33 | awk -F 'inet | brd' ' {print $2}'|sed -n 2p`
+export INTER_IPS=`ip addr | grep  "$INTER_NAME" | awk -F 'inet | brd' ' {print $2}'|sed -n 2p`
+export INTER_IP=`ip addr  | grep  "$INTER_NAME" | awk -F ' '  '/inet / {print $2}' |awk -F '/' '{print $1}'`
 export GATEWAY=`route -n  | grep UG |awk '{print $2}'`
 echo "当前系统 $OS_NAME_VERSION"
 #通用变量
