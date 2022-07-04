0806791c7924fb42f95a53436033ae868def1d0a
FaithOS <273101165@qq.com>
2022-07-04 08:32:54 +0800

    完善 历史命令脚本

--- scripts/Ubuntu_18.04/scripts_dir_list/Personal/set_history.sh
+++ scripts/Ubuntu_18.04/scripts_dir_list/Personal/set_history.sh
@@ -1,8 +1,13 @@
 #/bin/bash
 # 设置历史命令执行时间 set_history.sh
-
-# chmod o+w 表示 普通用户也有对该文件写的权限，这样普通用户的操作也能被记录到该文件内，缺点：普通用户可以修改该文件
+set -e
 #历史命令记录日志
+#1. 多用户记录操作命令
+#2. 操作记录，记录在日志内
+#3. 按执行时间、登入IP、登入终端方式、用户名、执行内容 进行记录
+#4. 注意 交互式登入和非交互式登入 都要记录操作内容
+#缺点
+# 远程用户使用su - 普通用户 不会记录IP 但是可以通过日志来确认，因为使用root登入的时候会记录IP
 #禁止cat 解析变量
 #cat >/etc/bash.bashrc<<-"EOF"
   [  ! -f  /var/log/history.log ] && touch /var/log/history.log
@@ -20,5 +25,8 @@ PROMPT_COMMAND=”history -a”
 #在脚本内使用bash 生效环境变量， 当前标签不生效， 需要重新打开一个新的标签就会生效了
 bash /etc/profile.d/history.sh
 #防止普通用户删除或查看日志文件
-chmod 642 $HISTORY_FILE
-echo "历史命令设置完毕，当前标签不生效， 需要重新打开一个新的连接标签就会生效."
+# chmod o+w 表示 普通用户也有对该文件写的权限，这样普通用户的操作也能被记录到该文件内，缺点：普通用户可以修改该文件
+if [[ "$NUM_FILE"  -ne 0642   ]] ;then
+chmod 642 /var/log/history.log
+echo -e "\033[31m  "历史命令设置完毕，当前标签不生效， 需要重新打开一个新的连接标签就会生效." \033[0m"
+fi
