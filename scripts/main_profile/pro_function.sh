3e4f43ce681c1235c112c950f8d00dbe31f942c6
FaithOS <273101165@qq.com>
2022-07-05 20:41:19 +0800

    添加clean函数

--- scripts/main_profile/pro_function.sh
+++ scripts/main_profile/pro_function.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 CLEAN(){
-
+# 由于在脚本内使用tar命令 中使用* 通配符， 在shell 执行流程上如果/tmp 路径下有多个文件 会被解析为 'file1' 'file2',这个多个文件， 里面的* 会被解释为'/tmp/*',导致通配符无法使用，所以定义成变量的方式来使用
+filename_tmp="${TMPDIR}/*"
 TMP_NUM=`ls -A "${TMPDIR}"|wc -l`
-#if [[ -n ${TMP_NUM} ]];then
-if [[ "${TMP_NUM}" -gt 1 ]];then
-    tar cvf  /tmp/scripts_tmp_$(date +%H:%M:%S).tar.gz ./tmp/*  --remove-files ./tmp/* 
+if [[ "${TMP_NUM}" -gt 0 ]];then
+# --remove-files  表示在/tmp 创建一个scripts_tmp的包， 并且删除 ./tmp 目录下被打包的源文件
+  tar cvf  /tmp/scripts_tmp_$(date +%H-%M-%S).tar.gz $filename_tmp  --remove-files $filename_tmp
 else
     echo "${TMPDIR} 目录下没有文件"
  retun=$?    
 fi
 
-#tar cvf  /tmp/scripts_tmp_$(date +%H:%M:%S).tar.gz ./tmp/*  --remove-files ./tmp/* 
 }
