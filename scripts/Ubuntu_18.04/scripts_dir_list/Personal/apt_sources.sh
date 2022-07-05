f706de1854362ea3436422d6d158ffdc47c519f0
FaithOS <273101165@qq.com>
2022-07-05 17:13:47 +0800

    完善apt 源

--- scripts/Ubuntu_18.04/scripts_dir_list/Personal/apt_sources.sh
+++ scripts/Ubuntu_18.04/scripts_dir_list/Personal/apt_sources.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# 配置apt-get源 apt-source.sh
-
-#
+# 配置清华源和阿里云apt源 apt_sources.sh
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
-
 cat >/etc/apt/sources.list<<-EOF
 # 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
@@ -20,5 +17,28 @@ deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
 EOF
 
-apt-get update
+cat > /etc/apt/sources.list.d/aliyun.list <<EOF
 
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+
+# deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+
+# deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+
+EOF
+echo "配置完成开始更新apt源"
+apt update
