5a8491ee0e3a6a3299e37b08d17c9e1bb22ef67c
FaithOS <273101165@qq.com>
2022-07-07 12:45:51 +0800

    完善脚本提示符

--- scripts/Ubuntu_18.04/scripts_dir_list/Personal/tishifu.sh
+++ scripts/Ubuntu_18.04/scripts_dir_list/Personal/tishifu.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-# 命令提示符 tishifu.sh
+# Ubuntu命令提示符 tishifu.sh
+set -e
 
-#shopt -s xpg_echo
-echo  'export PS1='\'\[\\u\@\\h \\W\]\\\$' '\' >> /root/.bashrc
-source /root/.bashrc
-#echo "这是测试脚本"
-#fuwuqi_info=`dmidecode|grep "System Information" -A9|egrep "Manufacturer|Product|Serial"`
-#echo $fuwuqi_info
-#a=($fuwuqi_info)
-#echo $a
-#b=${#fuwuqi_info[@]}	
-#echo $b
+/bin/cp -r /etc/bash.bashrc /etc/bash.bashrc.backup
+sed -i  '21s/w/&]/g'  /etc/bash.bashrc
+sed -i  '21s/\}/&[/g'  /etc/bash.bashrc
+echo -e "\033[31m  "提示符配置完毕" \033[0m"  "\033[32m 脚本执行成功  [ OK ] \033[0m"
+echo -e "\033[31m  "在本地执行手动  source /etc/bash.bashrc  后生效" \033[0m"
