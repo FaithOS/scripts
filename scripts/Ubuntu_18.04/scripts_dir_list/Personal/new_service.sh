4b0afde89c923a692601b7f26d73cace0553e1d1
FaithOS <273101165@qq.com>
2022-07-08 21:19:00 +0800

    完善自动添加服务

--- scripts/Ubuntu_18.04/scripts_dir_list/Personal/new_service.sh
+++ scripts/Ubuntu_18.04/scripts_dir_list/Personal/new_service.sh
@@ -2,9 +2,8 @@
 # 添加新服务 new_service.sh
 set -e
 read -p "请输入你要创建的名称，小写字母，不能有空格": new_name
-echo '#'
 #添加二级菜单变量
-sed -i '/SSH_DIR/a\export '"${new_name}"'_DIR=${scripts_DIR}/${SDL}/'"${new_name}"'' ${scripts_DIR}/main_profile/my_profile.sh`
+sed -i '/SSH_DIR/a\export '"${new_name}"'_DIR=${scripts_DIR}/${SDL}/'"${new_name}"'' ${scripts_DIR}/main_profile/my_profile.sh
 /bin/cp -r  ${scripts_DIR}/${OS_NAME_VERSION}/SSH_list.sh ${scripts_DIR}/${OS_NAME_VERSION}/${new_name}_list.sh
 sed -i 's/SSH/'"${new_name}"'/g' ${scripts_DIR}/${OS_NAME_VERSION}/${new_name}_list.sh
 mkdir  ${scripts_DIR}/${SDL}/${new_name}
