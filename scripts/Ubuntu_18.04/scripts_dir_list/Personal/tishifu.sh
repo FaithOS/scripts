e4d85f4dc5fa80830dbdc1efa6de57f4bc30eb6a
FaithOS <273101165@qq.com>
2022-07-08 18:27:15 +0800

    完善提示符修改

--- scripts/Ubuntu_18.04/scripts_dir_list/Personal/tishifu.sh
+++ scripts/Ubuntu_18.04/scripts_dir_list/Personal/tishifu.sh
@@ -8,3 +8,4 @@ set -e
 sed -i '$a PS1=''"${debian_chroot:+($debian_chroot)}[\\u@\\h:\\w]\\$ "'' ' /root/.bashrc
 source /root/.bashrc
 color red  "提示符配置完毕,脚本执行成功  [ OK ] "
+color red  "脚本执行完后 需要重新开启一个标签才能生效"
