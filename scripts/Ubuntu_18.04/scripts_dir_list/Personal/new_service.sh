53aa6c8c5135ccad9b42c875e3323a692fa09be6
FaithOS <273101165@qq.com>
2022-07-08 20:11:33 +0800

    三级菜单

--- scripts/Ubuntu_18.04/scripts_dir_list/Personal/new_service.sh
+++ scripts/Ubuntu_18.04/scripts_dir_list/Personal/new_service.sh
@@ -5,14 +5,14 @@ read -p "请输入你要创建的名称，小写字母，不能有空格": new_n
 echo ${SDL}
 echo '#'
 #添加二级菜单变量
-sed -i '/SSHDIR/a\export '"${new_name}"'DIR=${scripts_DIR}/${SDL}/'"${new_name}"'' ${scripts_DIR}/main_profile/my_profile.sh
+sed -i '/SSH_DIR/a\export '"${new_name}"'_DIR=${scripts_DIR}/${SDL}/'"${new_name}"'' ${scripts_DIR}/main_profile/my_profile.sh`
 /bin/cp -r  ${scripts_DIR}/${OS_NAME_VERSION}/SSH_list.sh ${scripts_DIR}/${OS_NAME_VERSION}/${new_name}_list.sh
 sed -i 's/SSH/'"${new_name}"'/g' ${scripts_DIR}/${OS_NAME_VERSION}/${new_name}_list.sh
 mkdir  ${scripts_DIR}/${SDL}/${new_name}
 
 
-cat >>${scripts_DIR}/${SDL}/${new_name}/test.sh<<-EOF
+cat >>${scripts_DIR}/${SDL}/${new_name}/test.sh<<EOF
 #!/bin/bash
 # $「new_name}测试 test.sh
-echo -e "\033[31m ${new_name} 配置成功 " \033[0m"
+ color red  "${new_name} 配置成功 " 
 EOF
