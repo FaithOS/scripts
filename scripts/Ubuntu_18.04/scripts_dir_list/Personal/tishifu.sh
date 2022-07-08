b6473e819fdf84614419c9550eb9aa0c112ab22f
FaithOS <273101165@qq.com>
2022-07-08 18:25:12 +0800

    完善提示符修改

--- scripts/Ubuntu_18.04/scripts_dir_list/Personal/tishifu.sh
+++ scripts/Ubuntu_18.04/scripts_dir_list/Personal/tishifu.sh
@@ -2,8 +2,9 @@
 # Ubuntu命令提示符 tishifu.sh
 set -e
 #
-/bin/cp -r /etc/bash.bashrc /etc/bash.bashrc.backup
-sed -i  '21s/w/&]/g'  /etc/bash.bashrc
-sed -i  '21s/\}/&[/g'  /etc/bash.bashrc
-echo -e "\033[31m  "提示符配置完毕" \033[0m"  "\033[32m 脚本执行成功  [ OK ] \033[0m"
-echo -e "\033[31m  "在本地执行手动  source /etc/bash.bashrc  后生效" \033[0m"
+/bin/cp -r /root/.bashrc /root/.bashrc.backup
+#sed -i  '21s/w/&]/g'  /etc/bash.bashrc
+#sed -i  '21s/\}/&[/g'  /etc/bash.bashrc
+sed -i '$a PS1=''"${debian_chroot:+($debian_chroot)}[\\u@\\h:\\w]\\$ "'' ' /root/.bashrc
+source /root/.bashrc
+color red  "提示符配置完毕,脚本执行成功  [ OK ] "
